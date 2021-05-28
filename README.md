4591756c684e531ed98bf066bb2e40cfce4a7141
Vincent Emonet <vincent.emonet@gmail.com>
2021-05-28 10:22:57 +0200

    update URL

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # SPARQL endpoint for OpenPredict
 
-A SPARQL endpoint to serve predictions generated using the OpenPredict classifier.
+A SPARQL endpoint to serve predictions generated using the OpenPredict classifier, using custom SPARQL functions. Built with [rdflib-endpoint](https://github.com/vemonet/rdflib-endpoint)
 
-## Example queries 📬
+Access the SPARQL service endpoint at https://service.openpredict.137.120.31.102.nip.io/sparql
+
+OpenAPI docs at https://service.openpredict.137.120.31.102.nip.io
+
+
+## Available functions 🧪
+
+**<a href="https://yasgui.triply.cc/#query=PREFIX%20openpredict%3A%20%3Chttps%3A%2F%2Fw3id.org%2Fum%2Fopenpredict%2F%3E%0ASELECT%20%3FdrugOrDisease%20%3FpredictedForTreatment%20%3FpredictedForTreatmentScore%20WHERE%20%7B%0A%20%20%20%20BIND(%22OMIM%3A246300%22%20AS%20%3FdrugOrDisease)%0A%20%20%20%20BIND(openpredict%3Aprediction(%3FdrugOrDisease)%20AS%20%3FpredictedForTreatment)&endpoint=https%3A%2F%2Fservice.openpredict.137.120.31.102.nip.io%2Fsparql&requestMethod=GET&tabTitle=Query%209&headers=%7B%7D&contentTypeConstruct=application%2Fn-triples%2C*%2F*%3Bq%3D0.9&contentTypeSelect=application%2Fsparql-results%2Bjson%2C*%2F*%3Bq%3D0.9&outputFormat=table">Try the queries on YASGUI</a>**
 
 ### Get predictions
 
@@ -23,13 +30,13 @@ Use this federated query to retrieve predicted treatments for a drug or disease 
 PREFIX openpredict: <https://w3id.org/um/openpredict/>
 SELECT * WHERE
 {
-  SERVICE <https://sparql-openpredict.137.120.31.102.nip.io/sparql> {
+  SERVICE <https://service.openpredict.137.120.31.102.nip.io/sparql> {
 	SELECT ?drugOrDisease ?predictedForTreatment WHERE {
     	BIND("OMIM:246300" AS ?drugOrDisease)
     	BIND(openpredict:prediction(?drugOrDisease) AS ?predictedForTreatment)
 	}
   }
-}q
+}
 ```
 
 ## Install and run ✨️
--- app/main.py
+++ app/main.py
@@ -1,25 +1,13 @@
+from rdflib_endpoint import SparqlEndpoint
+
 import rdflib
 # from rdflib.plugins.sparql.evaluate import evalPart, evalBGP
 from rdflib.plugins.sparql.evalutils import _eval
 from rdflib import Graph, Literal, RDF, URIRef
-from rdflib.namespace import Namespace
+# from rdflib.namespace import Namespace
 
 from openpredict.openpredict_model import query_omim_drugbank_classifier
 from openpredict.openpredict_utils import init_openpredict_dir
-
-from rdflib_endpoint import SparqlEndpoint
-
-# EvalBGP https://rdflib.readthedocs.io/en/stable/_modules/rdflib/plugins/sparql/evaluate.html
-# Custom fct for rdf:type with auto infer super-classes: https://github.com/RDFLib/rdflib/blob/master/examples/custom_eval.py
-# BGP = Basic Graph Pattern
-# Docs rdflib custom fct: https://rdflib.readthedocs.io/en/stable/intro_to_sparql.html
-# StackOverflow: https://stackoverflow.com/questions/43976691/custom-sparql-functions-in-rdflib/66988421#66988421
-
-# Another project: https://github.com/bas-stringer/scry/blob/master/query_handler.py
-# https://www.w3.org/TR/sparql11-service-description/#example-turtle
-# Federated query: https://www.w3.org/TR/2013/REC-sparql11-federated-query-20130321/#defn_service
-# XML method: https://rdflib.readthedocs.io/en/stable/apidocs/rdflib.plugins.sparql.results.html#module-rdflib.plugins.sparql.results.xmlresults
-
 
 def most_similar(query_results, ctx, part, eval_part):
     """
@@ -31,25 +19,27 @@ def most_similar(query_results, ctx, part, eval_part):
         BIND("OMIM:246300" AS ?drugOrDisease)
         BIND(openpredict:most_similar(?drugOrDisease) AS ?mostSimilar)
     """
-    argument1 = str(_eval(part.expr.expr[0], eval_part.forget(ctx, _except=part.expr._vars)))
-    argument2 = str(_eval(part.expr.expr[1], eval_part.forget(ctx, _except=part.expr._vars)))
+    argumentEntity = str(_eval(part.expr.expr[0], eval_part.forget(ctx, _except=part.expr._vars)))
+    try:
+        argumentLimit = str(_eval(part.expr.expr[1], eval_part.forget(ctx, _except=part.expr._vars)))
+    except:
+        argumentLimit = None
     
-    similarity_results = {}
-    # TODO: get similarity from dataframe
+    # TODO: get similarity from dataframe using argumentEntity
+    # Using stub data
+    similarity_results = [{'mostSimilar': 'DRUGBANK:DB00001', 'score': 0.42}]
     
     evaluation = []
     scores = []
     for most_similar in similarity_results:
-        # Quick fix to get results for drugs or diseases
-        # evaluation.append(most_similar['entity'])
-        # scores.append(most_similar['score'])
-        evaluation.append(str(argument1) + str(argument2))
+        evaluation.append(most_similar['mostSimilar'])
+        scores.append(most_similar['score'])
 
     # Append the results for our custom function
     for i, result in enumerate(evaluation):
         query_results.append(eval_part.merge({
             part.var: Literal(result), 
-            # rdflib.term.Variable(part.var + 'Score'): Literal(scores[i])
+            rdflib.term.Variable(part.var + 'Score'): Literal(scores[i])
         }))
     return query_results, ctx, part, eval_part
 
@@ -87,35 +77,14 @@ def get_predictions(query_results, ctx, part, eval_part):
     return query_results, ctx, part, eval_part
 
 
-def custom_concat(query_results, ctx, part, eval_part):
-    """
-    Concat 2 string and return the length as additional Length variable
-
-    Query:
-    PREFIX myfunctions: <https://w3id.org/um/sparql-functions/>
-    SELECT ?concat ?concatLength WHERE {
-        BIND("First" AS ?first)
-        BIND(myfunctions:custom_concat(?first, "last") AS ?concat)
-    }
-    """
-    argument1 = str(_eval(part.expr.expr[0], eval_part.forget(ctx, _except=part.expr._vars)))
-    argument2 = str(_eval(part.expr.expr[1], eval_part.forget(ctx, _except=part.expr._vars)))
-    evaluation = []
-    scores = []
-    concat_string = argument1 + argument2
-    reverse_string = argument2 + argument1
-    evaluation.append(concat_string)
-    evaluation.append(reverse_string)
-    scores.append(len(concat_string))
-    scores.append(len(reverse_string))
-    # Append the results for our custom function
-    for i, result in enumerate(evaluation):
-        query_results.append(eval_part.merge({
-            part.var: Literal(result), 
-            rdflib.term.Variable(part.var + 'Length'): Literal(scores[i])
-        }))
-    return query_results, ctx, part, eval_part
-
+example_query = """Example query:\n
+```
+PREFIX openpredict: <https://w3id.org/um/openpredict/>
+SELECT ?drugOrDisease ?predictedForTreatment ?predictedForTreatmentScore WHERE {
+    BIND("OMIM:246300" AS ?drugOrDisease)
+    BIND(openpredict:prediction(?drugOrDisease) AS ?predictedForTreatment)
+}
+```"""
 
 init_openpredict_dir()
 # Start the SPARQL endpoint based on a RDFLib Graph
@@ -125,10 +94,11 @@ app = SparqlEndpoint(
     functions={
         'https://w3id.org/um/openpredict/prediction': get_predictions,
         'https://w3id.org/um/openpredict/most_similar': most_similar,
-        'https://w3id.org/um/sparql-functions/custom_concat': custom_concat
     },
-    title="SPARQL endpoint for RDFLib graph", 
-    description="A SPARQL endpoint to serve machine learning models, or any other logic implemented in Python. \n[Source code](https://github.com/vemonet/rdflib-endpoint)",
+    title="SPARQL endpoint for OpenPredict functions", 
+    description="A SPARQL endpoint to serve predictions generated using the OpenPredict classifier, using custom SPARQL functions. \n[Source code](https://github.com/MaastrichtU-IDS/openpredict-sparql-service)",
     version="0.0.1",
-    cors_enabled=True
+    public_url='https://service.openpredict.137.120.31.102.nip.io/sparql',
+    cors_enabled=True,
+    example_query=example_query
 )
